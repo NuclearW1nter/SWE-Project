88f1dcef2b7a20860e3eebf58dc4a5f12e7717fa
Justin Plassmeyer <jplassme@masonlive.gmu.edu>
2017-10-04 08:31:38 -0400

    second commit

--- HW 4.js
+++ HW 4.js
@@ -26,6 +26,67 @@ class Diablo_Character_Data
             }
             characterMap.set(charID,charName);
         }
+        return characterMap
+    }
+    LastPlayed()
+    {
+        var lastTime = [];
+        var lastThree;
+        for (let iter in this.D3_heroes){
+            for(let iterator in this.D3_heroes[iter]){
+                if (iterator === "last-updated"){
+                    lastTime.push(this.D3_heroes[iter][iterator]);
+                }
+            }
+        }
+        lastTime.sort();
+        lastThree = lastTime.slice(0,4);
+        return lastThree;
+    }
+    MostPlayed()
+    {
+        var classMostPlayed;
+        var currentMostPlayed;
+        var tempClass;
+        for (let iter in this.D3_heroes["seasonalProfiles"]["season0"]["timePlayed"]){
+            if (iter === "barbarian"){
+                classMostPlayed = iter;
+                currentMostPlayed = this.D3_heroes["seasonalProfiles"]["season0"]["timePlayed"][iter];
+            }
+            else{
+                tempClass = this.D3_heroes["seasonalProfiles"]["season0"]["timePlayed"][iter];
+                if (tempClass >= currentMostPlayed){
+                    currentMostPlayed = tempClass;
+                    classMostPlayed = iter;
+                }
+            }
+        }
+        return classMostPlayed;
     }
 }
 
+class HeroData{
+    constructor(data){
+        this.characterData = data;
+    }
+    GeneralInfo(){
+        var infoList = [];;
+        var infoHeaders = [];
+        for (let iter in this.characterData){
+            if (iter === "kills"){
+                infoHeaders.push(iter);
+                infoList.push(this.characterData[iter]["elites"]);
+            }
+            else{
+                infoHeaders.push(iter);
+                infoList.push(this.characterData[iter]);
+            }
+        }
+        infoList = infoList.slice(0,10);
+        infoHeaders = infoHeaders.slice(0,10);
+        var retrunArray =[];
+        retrunArray.push(infoHeaders);
+        retrunArray.push(infoList);
+        return retrunArray;
+    }
+}
